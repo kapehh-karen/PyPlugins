d1f69581a6040b6ee613602544dce18783dd4247
kapehh-karen <git@kapehh.net>
2016-09-26 18:47:33 +0300

    next stage

--- src/me/kapehh/net/pyplugins/Main.java
+++ src/me/kapehh/net/pyplugins/Main.java
@@ -1,45 +1,74 @@
 package me.kapehh.net.pyplugins;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.*;
-import org.bukkit.plugin.EventExecutor;
-import org.bukkit.plugin.RegisteredListener;
+import me.kapehh.net.pyplugins.EventWrappers.BukkitEvents;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.python.util.PythonInterpreter;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.logging.Logger;
 
 /**
  * Created by karen on 26.09.2016.
  */
 public class Main extends JavaPlugin {
+    private PythonInterpreter pythonInterpreter;
 
     @Override
     public void onEnable() {
-        getLogger().info("Loading...");
-        //getServer().getPluginManager().registerEvents(new ProxyListener(), this);
+        Logger log = getLogger();
+        log.info("Loading PyPlugins...");
 
-        /*final ProxyListener pyListener = new ProxyListener();
-        ExtendRegisteredListener registeredListener = new ExtendRegisteredListener(pyListener, new EventExecutor() {
-            @Override
-            public void execute(Listener listener, Event event) throws EventException {
-                ((ProxyListener) listener).onEvent(event);
-                System.out.println("EXECUTE: " + event.getClass().getName());
-                //pyListener.onEvent(event);
+        pythonInterpreter = new PythonInterpreter();
+
+        // Чекаем PyПлагины
+        if (!getDataFolder().exists())
+            getDataFolder().mkdirs();
+        for (File file : getDataFolder().listFiles()) {
+            if (file.isDirectory()) {
+                log.info("Loading PyPlugin: " + file.getName());
+                File pyMain = new File(file, "main.py");
+                if (!pyMain.exists()) {
+                    log.warning("[-] File 'main.py' in '" + file.getName() + "' not found!");
+                    continue;
+                }
+                try {
+                    pythonInterpreter.execfile(new FileInputStream(pyMain));
+                    log.info("[+] Successful loaded PyPlugin: " + file.getName());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
-        }, EventPriority.NORMAL, this, false *//* ignore cancelled - fast *//*);
+        }
 
-        for(HandlerList handler : HandlerList.getHandlerLists()) {
-            //System.out.println(handler);
-            //System.out.println(Arrays.toString(handler.getRegisteredListeners()));
-            //handler.register(registeredListener);
-        }*/
+        // Регистрируем все Bukkit события
+        getServer().getPluginManager().registerEvents(new BukkitEvents(), this);
+    }
 
-        getServer().getPluginManager().registerEvents(new ProxyListener(), this);
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        if (cmd.getName().equalsIgnoreCase("pyplugins")) {
+            if (args.length < 2) {
+                return false;
+            }
+
+            String action = args[0];
+            String pyginName = args[1];
+
+            if (action.equalsIgnoreCase("reload")) {
+
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void onDisable() {
-
+        pythonInterpreter.cleanup();
+        pythonInterpreter.close();
     }
 
 }
---
+++ src/me/kapehh/net/pyplugins/PyPluginsCore.java
@@ -0,0 +1,7 @@
+package me.kapehh.net.pyplugins;
+
+/**
+ * Created by karen on 26.09.2016.
+ */
+public class PyPluginsCore {
+}
